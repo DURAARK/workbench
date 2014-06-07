d23ad69e9674a9ceb91ac6acc6636b09a7a6bc44
Martin Hecher <martin.hecher@fraunhofer.at>
2014-06-07 19:39:31 +0200

    * express is serving Besnik's SDO-WebGUI * added a link to the SDO-WebGUI in the sidebar

--- server/app.js
+++ server/app.js
@@ -20,30 +20,26 @@ var Workbench = require('./core/workbench');
 var app = express();
 
 app.configure(function() {
-	app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 3000);
 
-	app.set('view engine', 'handlebars');
-	app.set('views', __dirname + '../app/scripts/views');
+    app.set('view engine', 'handlebars');
+    app.set('views', __dirname + '../app/scripts/views');
 });
 
 // mount static
 app.use(express.static(path.join(__dirname, '../app')));
 app.use(express.static(path.join(__dirname, '../.tmp')));
-
-// route index.html
-app.get('/', function(req, res) {
-	res.sendfile(path.join(__dirname, '../app/index.html'));
-});
+app.use(express.static(path.join(__dirname + '../app/modules/contrib/sdo')));
 
 // set logging
 app.use(function(req, res, next) {
-	console.log('%s %s', req.method, req.url);
-	next();
+    console.log('%s %s', req.method, req.url);
+    next();
 });
 
 // start server
 http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express App started!');
+    console.log('Express App started!');
 });
 
 /* ----------------------------------------------------------------------------
