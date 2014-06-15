e7af86bbdb1c8c4841f8114c32765a0f8a5c4904
dagedv <dag.fjeld.edvardsen@catenda.no>
2014-06-16 01:19:37 +0200

    Added (too) simple file upload

--- server/app.js
+++ server/app.js
@@ -9,13 +9,19 @@ var baucis = require('baucis');
 // var rdf2json = require('./node-rdf2json/lib/rdf2json/node-rdf2json/index.js');
 var sys = require('sys');
 var fs = require('fs');
+var url = require("url");
+var multipart = require("multipart");
 var ServiceProviderMixin = require('./core/service-provider-mixin/index.js');
 var _config = require(__dirname + '/../package.json');
 var Workbench = require('./core/workbench');
-
+//var JSON 
 /* ----------------------------------------------------------------------------
  * Webserver setup
  * --------------------------------------------------------------------------*/
+
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util')     ;
 
 var app = express();
 
@@ -26,20 +32,41 @@ app.configure(function() {
     app.set('views', __dirname + '../app/scripts/views');
 });
 
+
+//for file upload
+ app.use(express.bodyParser( {uploadDir: path.join(__dirname, '../server/uploads'), keepExtensions: true } )); //puts the parsed body in req.body
+// app.post("*", function(req,res){
+// 	res.end( JSON.stringify(req.files) + "+n" );
+// } );
+
+
+
 // mount static
 app.use(express.static(path.join(__dirname, '../app')));
 app.use(express.static(path.join(__dirname, '../.tmp')));
 app.use(express.static(path.join(__dirname + '../app/modules/contrib/sdo')));
 
-// set logging
-app.use(function(req, res, next) {
-    console.log('%s %s', req.method, req.url);
-    next();
+
+//TODO: make ajaxy
+app.post('/upload',function(req,res){
+    console.log('FIRST TEST: ' + JSON.stringify(req.files));
+    console.log('second TEST: ' +req.files.theFile.name);
+    fs.readFile(req.files.theFile.path, function (err, data) {
+        var newPath = "/home/dagedv/Skrivebord/stuff/"+req.files.theFile.name;
+        fs.writeFile(newPath, data, function (err) {
+          res.send('/ POST OK');
+        });
+    });
 });
+
+
 
 // start server
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express App started!');
+
+
+
 });
 
 /* ----------------------------------------------------------------------------
