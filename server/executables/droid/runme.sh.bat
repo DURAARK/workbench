85798d271af174b27d365efc044e6d46bb027777
dagedv <dag.fjeld.edvardsen@catenda.no>
2014-07-01 13:54:14 +0200

    This bash/bat file actually runs Droid v6.1.4/6.2.0 happily against a single file without it complaining
    Former-commit-id: 6180a768a94aa3e326f1175a199e1e60fee047f0

--- server/executables/droid/runme.sh.bat
+++ server/executables/droid/runme.sh.bat
@@ -1,1 +1,3 @@
-java -jar droid-command-line-6.2.0-SNAPSHOT.jar -q -R -Nr src -Ns DROID_SignatureFile_V74.xml
+java -jar ./server/executables/droid/droid-command-line-6.2.0-SNAPSHOT.jar -q -R -Nr ./server/executables/droid/src/duraark_logo.png -Ns ./server/executables/droid/DROID_SignatureFile_V74.xml
+
+
