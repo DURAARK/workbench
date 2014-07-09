3f94302859d96d92ee70b0f48949e05150ed5dda
dagedv <dag.fjeld.edvardsen@catenda.no>
2014-07-09 16:19:30 +0200

    Made Sipgenerator-screen ready for presenting models (currently Probado used as placeholder). Purpose: to show files that will be entered into SIP.
    Former-commit-id: 7616bd5b28936467d5c0cba950e2c373e063217d

--- app/modules/contrib/sipgenerator/main.js
+++ app/modules/contrib/sipgenerator/main.js
@@ -1,4 +1,4 @@
-define([
+/*define([
     'backbone.marionette',
     'workbenchui',
     'core/uimodulebase',
@@ -48,4 +48,88 @@ define([
     // NOTE: No explicit return value is given here vor the AMD module. The module
     // is registered with the Marionette.Application and accessible via its
     // WorkbenchUI.module('...') syntax.
+});*/
+
+//==========================
+
+define([
+    'backbone',
+    'backbone.marionette',
+    'workbenchui',
+    'core/uimodulebase',
+    './mainview.js'
+], function(Backbone, Marionette, WorkbenchUI, UIModuleBase, MainView) {
+
+    WorkbenchUI.module('Contrib.SIPGenerator', UIModuleBase);
+
+    var MyModule = WorkbenchUI.module("Contrib.SIPGenerator");
+
+    WorkbenchUI.addInitializer(function() {
+        // 1. Register module with the ModuleManager:
+        WorkbenchUI.execute('module:register', 'Contrib.SIPGenerator');
+
+ // 2. Register eventhandler to show the view:
+        WorkbenchUI.vent.on('module:sipgenerator:show', function(aSearchterm) {
+            console.log('module:sipgenerator:show');
+            // First create the Model classes:
+            var SemObsModel = Backbone.Model.extend({                
+                urlRoot: "/services/probado" //TODO: replace this with somthing .. list of files from "Content"??
+            });
+
+          
+            if (!MyModule._mainView) {
+                // Create emtpy main view and show it:
+                MyModule._mainView = new MainView();
+
+                if (typeof region !== 'undefined') {
+                    region.show(MyModule._mainView);
+                } else {
+                    this.mainRegion.show(MyModule._mainView);
+                }
+                
+                
+                // Use the WorkbenchUI.fetchModel() method here to grab the model with id 1. In the 'then' function 
+                // callback it is guaranteed the the data from the server is received and the model is accessible:
+                //WorkbenchUI.fetchModel(BuildmModel, 1).then(function(model) {
+                WorkbenchUI.fetchModel(SemObsModel,"start=0&count=10").then(function(model) {
+                    if(typeof model=="undefined"){
+                        alert('No meaningful result from endpoint. Maybe the endpoint is down?');
+                    };                  
+                    MyModule._mainView.updateBuildmData(model);
+                });
+
+            } else {
+                console.log("==> This is the else zone!!");
+
+                //*** TODO: / //FIXME: security..
+                WorkbenchUI.fetchModel(SemObsModel,"fulltextQuery=" + aSearchterm + "&start=0&count=10").then(function(model) { //search uses: fulltextQuery=Kamille&start=0&count=10
+                    if(typeof model=="undefined"){
+                        alert('No meaningful result from endpoint. Maybe the endpoint is down?');
+                    };    
+                    MyModule._mainView.updateBuildmData(model);
+                });
+
+                // For the first show in the lifetime of the _mainView the events hash is correctly
+                // evaluated. When the _mainView gets closed and is reopened again, the events
+                // have to be delegated manually, otherwise e.g. the click events will not fire.
+                // That's what the next line is for:
+                MyModule._mainView.delegateEvents();
+            }
+
+        }.bind(this));
+
+        console.log('[WorkbenchUI.Contrib.SIPGenerator] started');
+    });
+
+    // TODO: not working with this version of Marionette...
+    // MyModule.addFinalizer(function() {
+    //     WorkbenchUI.mainRegion.close();
+
+    //     console.log('[WorkbenchUI.Contrib.SearchAndRetrieve] stopped');
+    // });
+
+    // NOTE: No explicit return value is given here vor the AMD module. The module
+    // is registered with the Marionette.Application and accessible via its
+    // WorkbenchUI.module('...') syntax.
 });
+
--- app/modules/contrib/sipgenerator/mainview.js
+++ app/modules/contrib/sipgenerator/mainview.js
@@ -1,4 +1,4 @@
-define([
+/*define([
 	'backbone.marionette',
 	'workbenchui',
 	'hbs!./templates/main'
@@ -20,4 +20,108 @@ define([
 	});
 
 	return MainView;
+});*/
+
+//============================================================0
+
+define([
+    'backbone.marionette',
+    'workbenchui',
+    'hbs!./templates/main',
+    'hbs!./templates/list-item',
+    'hbs!./templates/list-collection'
+], function(Marionette, WorkbenchUI, SemanticObservatoryTmpl, ListItemTmpl, TableTmpl) {
+    // Represents on list item:
+    var ListItemView = Backbone.Marionette.ItemView.extend({        
+        template: ListItemTmpl,
+        tagName: 'tr',
+        events: {
+            "click td": "cellClicked"
+        },
+        initialize: function(options) {
+            console.log("options.myTableView = " + options.myTableView );         
+        },
+
+
+        fieldsChanged: function() {
+            this.render();
+        }
+    });
+
+    // Represents the table view, which is using the ListItemView to render its items:
+    var TableView = Backbone.Marionette.CompositeView.extend({
+        tagName: "table",
+        className: 'table table-striped',
+        template: TableTmpl,
+        itemView: ListItemView,
+        itemViewOptions: {
+            myTableView: this
+        },
+
+        appendHtml: function(collectionView, itemView) {
+            collectionView.$("tbody").append(itemView.el);
+        }
+    });
+
+    // Represents the main page, including the TableView in the 'list' region when the 'onShow' method is called:
+    var MainView = Backbone.Marionette.Layout.extend({
+        template: SemanticObservatoryTmpl,
+
+        regions: {
+            sipgen: "#sipgen-region" 
+        },
+
+        events: {
+        	'click .js-search': function() {   //TODO -- remove this one??
+                //Show spinner while we wait for search results from Probado             
+                document.getElementById("sipgen-region").innerHTML = '<img src="spinner.gif" alt="spinner"><p>Waiting for webservice to provide fresh info</p>';
+                var aSearchterm = $("#searchstringinput").val();
+                WorkbenchUI.vent.trigger('module:sipgenerator:show', aSearchterm);
+            },         
+			'click .js-upload': function() {
+				console.log('upload clicked');
+				alert('Rosetta-SIP generated and uploaded to Rosetta!');
+				WorkbenchUI.vent.trigger('module:searchandretrieve:show');
+			},
+			'click .js-previous': function() {
+				console.log('previous');
+				WorkbenchUI.vent.trigger('module:semanticenrichment:show');
+			}
+		
+        },
+
+        initialize: function() {
+
+        },
+
+        updateBuildmData: function(model) {
+            this.sipgen.show(new TableView({
+                collection: this._modelToCollection(model)
+            }));
+        },
+
+
+        _modelToCollection: function(model) {
+            // Here the given model is converted into a collection for easier display.
+            // CAUTION: After values have changed in the collection it is necessary to
+            // serialize the collection into a single model to store it back to the server!
+
+            var collection = new Backbone.Collection();
+            for (var key in model.attributes) {
+                // Skip 'id' attribute:
+                if (key === 'id') continue;
+
+                var value = model.attributes[key];
+                collection.push({
+                    key: key,
+                    value: value
+                });
+            };
+
+            return collection;
+        }
+    });
+
+    return MainView;
 });
+
