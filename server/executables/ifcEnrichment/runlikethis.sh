a012968601b36149c0faae5b06e048bd28f380a6
dagedv <dag.fjeld.edvardsen@catenda.no>
2014-07-11 01:39:54 +0200

    Bash file for running the ifcEnrichment.jar file
    Former-commit-id: e9b4522f8b8f719b8b460882c39d3d9891858f0d

--- server/executables/ifcEnrichment/runlikethis.sh
+++ server/executables/ifcEnrichment/runlikethis.sh
@@ -1,1 +1,2 @@
-java -jar -Xmx2048m  ifcEnrichment.jar ../../fixtures/repository/Duplex_A_20110907_optimized.ifc
+#requires that the file /home/<user>/ifcEnrichment/enrichmentTriples.txt exists .. this seems to be where (some of the?) results are stored.
+java -jar -Xmx2048m ifcEnrichment.jar ../../fixtures/repository/Duplex_A_20110907_optimized.ifc IFCPOSTALADDRESS
