821f96d0094f3759821da7b8b5e2bd0bf9197716
Martin Hecher <martin.hecher@fraunhofer.at>
2014-07-19 12:37:14 +0200

    ...
    Former-commit-id: cc9461589a4184124f0ea0a36b4def8e10ffbab5

--- server/services/e57m.js
+++ server/services/e57m.js
@@ -48,7 +48,8 @@ E57M.prototype.onClose = function(code, res) {
     // 'output_info' contains information on the output file that was created by the command:
     console.log('[E57M:onStdOut] exit code: ' + code);
 
-    fs.readFile(this._outputFile, 'utf8', function(err, data) {
+    // FIXXME!
+    fs.readFile(path.join(app_root, 'fixtures', 'e57_tmp_output.json'), 'utf8', function(err, data) {
         if (err) {
             console.log('[E57M:onStdOut] error reading file: ' + err);
             return;
